572301a35b0dd20301f19500947c278effda40fd
Dan Beccaria <beccdaniel@gmail.com>
2021-03-01 08:52:33 -0800

    First Commit: added base files for running react, thanks yarn

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import components here
+
+const App = () => {
+    return (
+        // components
+        <h1>Component Gallery</h1>
+    )
+};
+
+export default App;
