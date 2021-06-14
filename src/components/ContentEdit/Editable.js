dd6f77a1a10a32335b065af2d91fa3431eeb0f4f
dbeccaria826 <beccdaniel@gmail.com>
2021-06-14 11:03:34 -0700

    Add logic for checking user privilege

--- src/components/ContentEdit/Editable.js
+++ src/components/ContentEdit/Editable.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import ContentEditable from 'react-contenteditable';
+// Mock User
+const user = {
+  name: 'Admin',
+  group: 'Admin',
+};
 
 class MyComponent extends React.Component {
   constructor() {
     super()
     this.contentEditable = React.createRef();
-    this.state = {html: '<h1>Hello World </h1>'}
+    this.state = { html: '<h2>Hello World </h2>' }
+    console.log('the value of the ref', this.contentEditable.current);
   };
 
   handleChange = e => {
     this.setState({ html: e.target.value });
+    console.log('the ref change inside handleChange', this.contentEditable);
   };
 
+  checkUserPriv = (user) => {
+    if (user?.group === 'Admin') return true;
 
+    return false;
+  }
 
   render() {
     return <ContentEditable
       innerRef={this.contentEditable}
       html={this.state.html}
-      disabled={false}
+      disabled={this.checkUserPriv(user) ? false : true}
       onChange={this.handleChange}
       tagName="article"
     /> 
-  }
-}
+  };
+};
 
-export default MyComponent
+export default MyComponent;
