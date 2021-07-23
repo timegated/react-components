e5e76315d12d89a993d0478d80970cc97a321b54
timegated <beccdaniel@gmail.com>
2021-07-22 22:06:35 -0700

    Added spacingMap object for setting gutter values

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -3,8 +3,8 @@ import Stack from '../../composable-layout/Stack';
 
 const Newsletter = () => {
   return (
-    <div>
-      <Stack gutter="0.5rem">
+    <Stack gutter="xl">
+      <Stack gutter="sm">
         <h2>Subscribe to our Newsletter</h2>
         <p>
           Subscribe to our newsletter to keep up to date on all our amazing products
@@ -12,23 +12,20 @@ const Newsletter = () => {
       </Stack>
 
       <Stack>
-        <div>
           <Stack gutter="0.25rem">
             <label htmlFor="name">Name</label>
             <input type="text" id="name" />
           </Stack>
-        </div>
 
-        <div>
           <Stack gutter="0.25rem">
             <label htmlFor="email">Email</label>
             <input type="text" id="email" />
           </Stack>
-        </div>
+
 
         <button>Subscribe</button>
       </Stack>
-    </div>
+    </Stack>
   )
 }
 
--- src/composable-layout/Stack.js
+++ src/composable-layout/Stack.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// Based on t-shirt sizes
+const spacingMap = {
+  xs: "0.125rem",
+  sm: "0.25rem",
+  md: "0.5rem",
+  lg: "1rem",
+  xl: "2rem",
+  xxl: "4rem"
+};
+
 const Grid = styled.div`
   display: grid;
-  gap: ${(props) => props.gutter ?? "1rem"};
+  gap: ${(props) => spacingMap[props.gutter] ?? spacingMap.lg};
 `;
 
 const Stack = ({ children, gutter }) => {
